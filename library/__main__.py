cc43f0e9282ccec892da0b6b9852c97d20f84f70
mign0n <mign0n.git@gmail.com>
2024-07-24 18:36:50 +0500

    Remove the unnecessary logging messages.

--- library/__main__.py
+++ library/__main__.py
@@ -7,11 +7,9 @@ from model import Library
 def main() -> None:
     """Основная функция."""
     configure_logging()
-    logging.info('Library is running...')
 
     args = ConfigArgumentParser().parser.parse_args()
     args.func(Library(), args)
-    logging.info('Library is stopped.')
 
 
 if __name__ == '__main__':
